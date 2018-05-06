a5041264c9a9907a0a14ef9aeb8a7aa213c967e2
Lucas Rangit MAGASWERAN <lucasrangit@gmail.com>
2018-05-06 03:09:58 +0200

    save the votes based on the url visited, 0 deletes the vote, display the votes and tally the results. use CSS to fill the viewport to make it easier to read on mobile.

--- app.js
+++ app.js
@@ -15,24 +15,82 @@ async function get_mac(ip) {
     });
 };
 
+// votes are stored in RAM
+var votes = {};
+
+function render_results(req, res) {
+    let results = {};
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<!DOCTYPE html>\n<html>\n');
+    res.write('<head>\n<meta name="viewport" content="width=device-width, initial-scale=1">\n');
+    res.write('<style>\nh1, table {\n\tfont-size: 10vw;\n\tborder-collapse: collapse;\n}\ntable, th, td {\n\tborder: 1px solid #ddd;\n}\n</style></head>\n');
+    res.write('<p>Go to /NUMBER to cast or update your vote.</p>\n');
+    res.write('<p>Go to /0 to delete your vote.</p>\n');
+    res.write('<p>Go to / for the results.</p>\n');
+    res.write('<body>\n<h1>Votes</h1>\n');
+    res.write('<table>\n\t<tr>\n\t\t<th>MAC</th>\n\t\t<th>Vote</th>\n</tr>\n');
+    for (var voter in votes) {
+        let vote = votes[voter];
+        if (vote in results)
+            results[vote] += 1;
+        else
+            results[vote] = 1;
+        res.write('\t<tr>\n\t\t<td>' + voter + '</td>\n\t\t<td>' + votes[voter] + '</td>\n\t<tr>\n');
+    }
+    res.write('<table>\n');
+    res.write('<h1>Results</h1>\n');
+    res.write('<table>\n\t<tr>\n\t\t<th>Choice</th>\n\t\t<th>Count</th>\n</tr>\n');
+    for (var choice in results) {
+        res.write('\t<tr>\n\t\t<td>' + choice + '</td>\n\t\t<td>' + results[choice] + '</td>\n\t<tr>\n');
+    }
+    res.write('</table>\n</body>\n</html>');
+    res.end();
+    return;
+};
+
 http.createServer((async (req, res) => {
     console.log('Request from: ' + req.connection.remoteAddress);
-
-    // TODO avoid localhost no MAC issue and route to setup page first
 
     try {
         var mac = await get_mac(req.connection.remoteAddress);
     } catch (e) {
         console.log(e.name + ': ' + e.message);
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(e.name + ': ' + e.message + '\n');
+        render_results(req, res);
         return;
     }
 
-    res.writeHead(200, {'Content-Type': 'text/text'});
-    res.write('Hello ' + mac + '!\n');
-    res.end();
+    let url = req.url.replace(/^\/+/g, '');
+    switch (true) {
+        case (parseInt(url) > 0):
+            console.log('Vote: ' + url);
+            votes[mac] = url;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write('<!DOCTYPE html>\n<html>\n');
+            res.write('<head>\n<meta name="viewport" content="width=device-width, initial-scale=1">\n');
+            res.write('<style>\np {\n\tfont-size: 10vw;\n}\n\n</style></head>\n');
+            res.write('<body>\n');
+            res.write('<p>Hello ' + mac + '!</p>\n');
+            res.write('<p>Your vote for ' + url + ' has been recorded.</p>\n');
+            res.write('</body>\n</html>\n');
+            res.end();
+            break;
+        case (parseInt(url) == 0):
+            console.log('Vote: ' + url);
+            delete votes[mac];
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write('<!DOCTYPE html>\n<html>\n');
+            res.write('<head>\n<meta name="viewport" content="width=device-width, initial-scale=1">\n');
+            res.write('<style>\np {\n\tfont-size: 10vw;\n}\n\n</style></head>\n');
+            res.write('<body>\n');
+            res.write('<p>Hello ' + mac + '!</p>\n');
+            res.write('<p>Your vote has been deleted.</p>\n');
+            res.write('</body>\n</html>\n');
+            res.end();
+            break;
+        default:
+            console.log('NaN : ' + url + '\n');
+            render_results(req, res);
+    }
 })).listen(8080, '0.0.0.0'); // '0.0.0.0' forces IPv4 IP address (macfromip only supports IPv4)
 
 console.log("Server listening on port 8080\n");
